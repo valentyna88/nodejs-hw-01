106b1439547427e76da72bd690239b5600621e99
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-11-22 16:55:49 +0200

    Done tasks 1 and 2

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,11 @@
+import fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  const data = JSON.stringify(updatedContacts, null, 2);
+  try {
+    await fs.writeFile(PATH_DB, data, 'utf8');
+  } catch (error) {
+    console.error(error);
+  }
+};
