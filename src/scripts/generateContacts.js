b98101af932ceb7ee626bc5bf029cefab22a46ce
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-11-23 23:41:41 +0200

    Update files

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,9 @@
-const generateContacts = async (number) => {};
+import { readContacts } from '../utils/readContacts';
+
+const generateContacts = async (number) => {
+  try {
+    const existingContacts = await readContacts();
+  } catch (error) {}
+};
 
 generateContacts(5);
