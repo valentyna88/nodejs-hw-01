ca6107f046ca33894c84047691334e421f8ec11a
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-11-26 08:29:58 +0200

    Done task 3

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,9 +1,18 @@
-import { readContacts } from '../utils/readContacts';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
 
 const generateContacts = async (number) => {
   try {
     const existingContacts = await readContacts();
-  } catch (error) {}
+    const newContacts = Array.from({ length: number }, () =>
+      createFakeContact(),
+    );
+    const updatedContacts = [...existingContacts, ...newContacts];
+    await writeContacts(updatedContacts);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 generateContacts(5);
