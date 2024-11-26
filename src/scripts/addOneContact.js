dc099ac5bad9b58f324f798df08dc6a18ed9935a
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-11-26 09:56:23 +0200

    Done task 4

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,16 @@
-export const addOneContact = async () => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const addOneContact = async () => {
+  try {
+    const existingContacts = await readContacts();
+    const newContact = createFakeContact();
+    const updatedContacts = [...existingContacts, newContact];
+    await writeContacts(updatedContacts);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 addOneContact();
