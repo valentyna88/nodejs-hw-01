bc4646ad74a3848de626acbf32cbe85fc6da74c0
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-11-26 11:34:49 +0200

    Done tasks 7 and 8

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,19 @@
-export const removeLastContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const removeLastContact = async () => {
+  try {
+    const contacts = await readContacts();
+    if (contacts.length > 0) {
+      contacts.pop();
+      console.log('The last contact was removed');
+    } else {
+      console.log('No contacts');
+    }
+    await writeContacts(contacts);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 removeLastContact();
